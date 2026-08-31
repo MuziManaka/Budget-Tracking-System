3a098ffc45017e500521862d1ded66530f6a17bb
Muzi Manaka <BC-STUDENT@WIN-4A6UHFI011M>
2026-08-31 19:59:30 +0200

    models

---
+++ prg project/Models/Budget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg_project.Models
+{
+    internal class Budget
+    {
+        public 
+    }
+}
---
+++ prg project/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg_project.Models
+{
+    internal class User
+    {
+    }
+}
